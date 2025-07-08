655550d711b6075e93ae5820f3572a39849f1e54
Kirill <ananevka@mail.ru>
2025-07-08 15:36:31 +0300

    Compose UI

--- app/src/main/java/com/example/projectnailsschedule/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/projectnailsschedule/presentation/ui/theme/Color.kt
@@ -2,9 +2,21 @@ package com.example.projectnailsschedule.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Primary = Color(0xFF6200EE)
-val Secondary = Color(0xFF03DAC6)
-val Background = Color(0xFFFFFFFF)
-val Surface = Color(0xFFFFFFFF)
-val OnPrimary = Color(0xFFFFFFFF)
-val OnBackground = Color(0xFF000000)
+val AppPrimary = Color(0xFF74489F)         // Gradient5
+val AppPrimaryVariant = Color(0x4BEAE6EB)  // Pink7
+val AppOnPrimary = Color.White                    // Белый текст на primary
+
+val AppSecondary = Color(0xFFD19FCE)       // Pink3
+val AppSecondaryVariant = Color(0xFFD6D2E3)// Pink4
+val AppOnSecondary = Color(0xFF132438)     // Dark1
+
+val AppBackground = Color(0xFFFFFFFF)      // Белый фон
+val AppOnBackground = Color(0xFF000000)    // Чёрный текст
+
+val AppSurface = Color(0xFFFFFFFF)         // Белая "поверхность"
+val AppOnSurface = Color(0xFF000000)       // Чёрный текст на surface
+
+val AppButtonColor = Color(0xFFFFEB3B)     // Gradient6
+val AppOnButtonColor = Color(0xFF000000)   // Чёрный текст на жёлтой кнопке
+
+
--- app/src/main/java/com/example/projectnailsschedule/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/projectnailsschedule/presentation/ui/theme/Theme.kt
@@ -1,37 +1,36 @@
 package com.example.projectnailsschedule.presentation.ui.theme
 
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-private val LightColors = lightColorScheme(
-    primary = Primary,
-    secondary = Secondary,
-    background = Background,
-    surface = Surface,
-    onPrimary = OnPrimary,
-    onBackground = OnBackground
+val LightColorPalette = lightColorScheme(
+    primary = AppPrimary,
+    onPrimary = AppOnPrimary,
+    primaryContainer = AppPrimaryVariant,
+    onPrimaryContainer = AppOnPrimary,
+
+    secondary = AppSecondary,
+    onSecondary = AppOnSecondary,
+    secondaryContainer = AppSecondaryVariant,
+    onSecondaryContainer = AppOnSecondary,
+
+    background = AppBackground,
+    onBackground = AppOnBackground,
+
+    surface = AppSurface,
+    onSurface = AppOnSurface,
 )
 
 @Composable
 fun AppTheme(
-    useDarkTheme: Boolean = false, // позже можно заменить на dynamic
     content: @Composable () -> Unit
 ) {
-    val colorScheme = LightColors // можно переключать здесь на тёмную тему
-
-    val systemUiController = rememberSystemUiController()
-    SideEffect {
-        systemUiController.setSystemBarsColor(
-            color = colorScheme.primary
-        )
-    }
+    val colors =  LightColorPalette
 
     MaterialTheme(
-        colorScheme = colorScheme,
-        typography = AppTypography,
+        colorScheme = colors,
+        typography = Typography,
         content = content
     )
 }
